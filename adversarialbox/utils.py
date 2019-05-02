8ca9a4f67519e6d4c665286df9c08046b8a6cc6d
rajasekhar <rajasekhar@swecha.net>
2019-05-02 15:14:19 +0700

    Update utils.py

--- adversarialbox/utils.py
+++ adversarialbox/utils.py
@@ -33,7 +33,7 @@ def to_var(x, requires_grad=False, volatile=False):
     Varialbe type that automatically choose cpu or cuda
     """
     if torch.cuda.is_available():
-        x = x.cuda(device=0)
+        x = x.cuda(device=1)
     return Variable(x, requires_grad=requires_grad, volatile=volatile)
 
 
